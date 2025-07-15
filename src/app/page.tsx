cd4079af8bc19a19bc4e4f37c72bed691ac7a9ba
Ashish Lamichaney <124287828+A5shi5h@users.noreply.github.com>
2025-07-15 15:52:31 +0530

    gallery section and about section and some images added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 'use client'
 
 import React from 'react'
-import HeroSection from './components/HeroSection'
-import AboutSection from './components/AboutSection'
-import MenuSection from './components/MenuSection'
-import GallerySection from './components/GallerySection'
-import ContactSection from './components/ContactSection'
+import HeroSection from '../components/HeroSection'
+import AboutSection from '../components/AboutSection'
+import MenuSection from '../components/MenuSection'
+import GallerySection from '../components/GallerySection'
+import ContactSection from '../components/ContactSection'
 
 const Page = () => {
   return (
